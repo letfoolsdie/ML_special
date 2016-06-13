78a12b0c062ad6785314d3cec4024f3b2c77e332
Nikolay <letfoolsdie@gmail.com>
2016-06-13 14:54:25 +0300

    Added week 3 and 4 of the 3rd course

--- supervisedLearning/w4/grad_boost.py
+++ supervisedLearning/w4/grad_boost.py
@@ -38,25 +38,43 @@ from sklearn.linear_model import LinearRegression
 df = load_boston()
 X = df.data
 y = df.target
+X_train, y_train = X[:380], y[:380]
+X_test, y_test = X[380:], y[380:]
+#
+#base_algorithms_list = [DecisionTreeRegressor(max_depth=5, random_state=42) for _ in range(50)]
+#coefficients_list = [0.9] * 50
+#
+#def gbm_predict(X, i):
+#   return [sum([coeff * algo.predict([x])[0] for algo, coeff in zip(base_algorithms_list[0:i], coefficients_list[0:i])]) for x in X]
+#
+#s = y
+#
+#for inx, (algorithm, coefficient) in enumerate(zip(base_algorithms_list, coefficients_list)):
+#   algorithm.fit(X, s)
+##   s = (gbm_predict(X, inx + 1) - y)
+#   s = -1 * (gbm_predict(X, inx+1) - y)
+#   print(mean_squared_error(gbm_predict(X, inx + 1), y))
 
 
-base_algorithms_list = [DecisionTreeRegressor(max_depth=5, random_state=42) for _ in range(50)]
-coefficients_list = [0.9] * 50
 
-def gbm_predict(X, i):
-   return [sum([coeff * algo.predict([x])[0] for algo, coeff in zip(base_algorithms_list[0:i], coefficients_list[0:i])]) for x in X]
+import matplotlib.pyplot as plt
 
-s = y
+def gbm_predict(X):
+    return [sum([coeff * algo.predict([x])[0] \
+    for algo, coeff in zip(base_algorithms_list, coefficients_list)]) \
+        for x in X]
+mse = []
+base_algorithms_list = []
+coefficients_list = []
+clf = DecisionTreeRegressor(max_depth = 5, random_state = 42)
+for i in range(50):
+    grad =  y_train - gbm_predict(X_train)       # градиент - вектор прогнозов нового базового алгоритма
+    base_algorithms_list.append(clf.fit(X_train, grad))  # тренируем новый базовый алгоритм по ответам уже имеющейся композиции
+    coefficients_list.append(0.9)
+    mse.append(mean_squared_error(y_train, gbm_predict(X_train)))
 
-for inx, (algorithm, coefficient) in enumerate(zip(base_algorithms_list, coefficients_list)):
-   algorithm.fit(X, s)
-#   s = (gbm_predict(X, inx + 1) - y)
-   s = -1 * (gbm_predict(X, inx+1) - y)
-   print(mean_squared_error(gbm_predict(X, inx + 1), y))
-
-
-
-
+iters = [i for i in range(len(mse))]
+plt.plot(iters, mse)
 
 
 
